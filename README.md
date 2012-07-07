71d9a8c1bab6a7192a51c28a542471bfb89f6c9b
Robert Kubiak <r.kubiak@keksmafia.de>
2012-07-07 23:52:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Ruby on Rails Tutorial: sample app
+
+This is just a repository for the foloowing ruby tutorial:
+http://ruby.railstutorial.org/ruby-on-rails-tutorial-book
+
+So this will have nothing new
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '7e43f4062f30fdb676be8f40587f6d872a2fce45c2ea9ba2a07735bb65e5149d1b9d9a38a9aa46519d3f3529f32b2fe8487eee6ba9dd0b08462228edca93efbb'
